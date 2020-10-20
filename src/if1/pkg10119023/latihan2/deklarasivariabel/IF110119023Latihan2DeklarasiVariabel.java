16645e8d75cbd79a42523172d27fc216abc25597
Farhan <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-20 19:14:36 +0700

    Latihan 2 - Deklarasi Variabel

---
+++ src/if1/pkg10119023/latihan2/deklarasivariabel/IF110119023Latihan2DeklarasiVariabel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhammad Farhan R
+ * KELAS    : IF1
+ * NIM      : 10119023
+ * Deskripsi Program : Program latihan membuat dan memberi nilai Variabel
+ */
+public class IF110119023Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan
+        System.out.println();
+        System.out.println("Isi variabel nilai = "+ nilaiInt);
+        System.out.println("Isi variabel PHI = "+ PHI);
+        System.out.println("Isi variabel logika = "+ nilaiLogika);
+        System.out.println("Isi variabel Karakter = "+ nilaiKarakter);
+        
+    }
+    
+}
